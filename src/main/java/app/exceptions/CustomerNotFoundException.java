8b6ac953d4660557a4f3594082c3ee4a31007e74
orekhov-vitaly <orekhov.vitaly@gmail.com>
2025-09-10 18:34:42 +0200

    Task 3 - Add service

---
+++ src/main/java/app/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package app.exceptions;
+
+public class CustomerNotFoundException extends Exception{
+    public CustomerNotFoundException(int id) {
+        super(String.format("Покупатель с идентификатором %d не найден", id));
+    }
+}
